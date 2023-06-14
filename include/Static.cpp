02a40edbc93ded4c41b60ffd15b0051e3bfa7340
Alexandre Cortez Martins <alexandre.martins@iurtsbrcsp.onmicrosoft.com>
2023-06-14 00:09:28 -0300

    first prototype with minimal code

---
+++ include/Static.cpp
@@ -0,0 +1,30 @@
+typedef struct
+{
+    int song;
+    char *RFIDUID;
+    char *text;
+    int color;
+} RFIDDictionary;
+
+const RFIDDictionary cards[20]{
+    {1, (char *)" 76 03 23 D0", (char *)"CUP OF WATER", 0xF800},
+    {2, (char *)" D3 82 23 D0", (char *)"BOTTLE OF WATER", 0xF800},
+    {3, (char *)" 7D 86 23 D0", (char *)"BOTTLE OF JUICE", 0xF800},
+    {4, (char *)" E1 97 22 D0", (char *)"PLASTIC", 0xF800},
+    {5, (char *)" 37 D2 22 D0", (char *)"APPLE", 0x9A60},
+    {6, (char *)" 72 B9 23 D0", (char *)"BANANA", 0x9A60},
+    {7, (char *)" BA 32 2F 14", (char *)"FOOD", 0x9A60},
+    {8, (char *)" F5 CB 23 D0", (char *)"ORANGE", 0x9A60},
+    {9, (char *)" 3D 38 23 D0", (char *)"CAN OF BEER", 0xFFE0},
+    {10, (char *)" FA 88 22 D0", (char *)"CAN OF SODA", 0xFFE0},
+    {11, (char *)" 2A E5 6D CA", (char *)"CAN OF JUICE", 0xFFE0},
+    {12, (char *)" CE 20 23 D0", (char *)"PICTURE FRAME", 0xFFE0},
+    {13, (char *)" 79 7B 23 D0", (char *)"GLASS", 0x07E0},
+    {14, (char *)" 79 9F 22 D0", (char *)"none 1", 0x07E0},
+    {15, (char *)" 05 CD 23 D0", (char *)"none 2", 0x07E0},
+    {16, (char *)" EB 6F 23 D0", (char *)"none 3", 0x07E0},
+    {17, (char *)" 8C 8F 22 D0", (char *)"none 4", 0x001F},
+    {18, (char *)" A2 30 23 D0", (char *)"none 5", 0x001F},
+    {19, (char *)" AD F9 22 D0", (char *)"none 6", 0x001F},
+    {20, (char *)" BA A0 EA 14", (char *)"none 7", 0x001F},
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,131 @@
-#include <Arduino.h>
+#include "Static.cpp"
+#include "Util.cpp"
 
-// put function declarations here:
-int myFunction(int, int);
+void printDetail(int type, int value);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// MFRC522
+#include <Wire.h>
+#include <MFRC522v2.h>
+#include <MFRC522Debug.h>
+#include <MFRC522DriverSPI.h>
+#include <MFRC522DriverPinSimple.h>
+
+MFRC522DriverPinSimple ss_pin(15);
+MFRC522DriverSPI driver{ss_pin};
+MFRC522 mfrc522{driver};
+
+String tagContent = "";
+
+// TFT ST7735
+#include <SPI.h>
+#include <TFT_eSPI.h>
+
+TFT_eSPI tft = TFT_eSPI();
+
+// DFPlayer Mini
+#include "HardwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+
+const byte RXD2 = 17;
+const byte TXD2 = 16;
+const byte dfBusyPin = 22;
+HardwareSerial dfSD(1);
+DFRobotDFPlayerMini DFPlayer;
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial)
+    ;
+
+  // MFRC522 setup
+  mfrc522.PCD_Init();
+  MFRC522Debug::PCD_DumpVersionToSerial(mfrc522, Serial);
+  Serial.println();
+  Serial.println(F("Scan the RFID card to see the UID..."));
+
+  // TFT ST7735 setup
+  tft.init();
+  tft.setRotation(0);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextWrap(true, true);
+
+  // DFPlayer Mini setup
+  dfSD.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  Serial.println();
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+
+  delay(5000);
+  if (!DFPlayer.begin(dfSD))
+  {
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while (true)
+      ;
+  }
+  Serial.println(F("DFPlayer Mini online."));
+
+  pinMode(dfBusyPin, INPUT);
+  DFPlayer.setTimeOut(500);
+  DFPlayer.volume(10); // Volume value. From 0 to 30
+  DFPlayer.EQ(0);      /*  0 = Normal, 1 = Pop, 2 = Rock, 3 = Jazz, 4 = Classic, 5 = Bass */
+  DFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  // Read DFPlayer status
+  if (DFPlayer.available())
+  {
+    printDetail(DFPlayer.readType(), DFPlayer.read());
+  }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial() || !digitalRead(dfBusyPin))
+  {
+    delay(250);
+    return;
+  }
+
+  // Get RFID card UID
+  tagContent = "";
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    tagContent.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    tagContent.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  tagContent.toUpperCase();
+  Serial.println(tagContent);
+
+  // Loop trought card info
+  for (int i = 0; i < (sizeof(cards) / sizeof(int)) - 1; i++)
+  {
+    if (strcmp(cards[i].RFIDUID, tagContent.c_str()) == 0)
+    {
+      // Break if file count is lower than current index
+      if ((i + 1) > DFPlayer.readFileCounts())
+      {
+        break;
+      }
+
+      DFPlayer.play(cards[i].song);
+
+      // Write UID to screen
+      tft.setCursor(0, 0);
+      tft.fillScreen(TFT_BLACK);
+      tft.setTextSize(1);
+      tft.setTextColor(TFT_WHITE, TFT_BLACK);
+      tft.println("TAG UID: ");
+      tft.println(tagContent);
+
+      // Write card info
+      tft.println("");
+      tft.setTextColor(cards[i].color, TFT_BLACK);
+      tft.println(cards[i].text);
+      tft.fillRectVGradient(0, 50, 128, 128, TFT_BLACK, cards[i].color);
+
+      break;
+    }
+  }
 }
